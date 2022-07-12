b28eab17f293d0161164809a53d9081a520161f8
ilham89 <ilham.kodingworks@gmail.com>
2022-07-12 15:50:07 +0700

    change value to env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/database";
 
 const config = {
-  apiKey: "AIzaSyDQ2J7ifRBf0cslgi7HvX19xgCt39Fex54",
-  authDomain: "ngetes-7e07f.firebaseapp.com",
-  databaseURL: "https://ngetes-7e07f-default-rtdb.firebaseio.com/",
-  projectId: "ngetes-7e07f",
-  storageBucket: "ngetes-7e07f.appspot.com",
-  messagingSenderId: "186797972663",
-  appId: "1:186797972663:web:8002e7cb93bd0f861c0132",
-  measurementId: "G-KJF6SS204D",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 if (!firebase.apps.length) {
